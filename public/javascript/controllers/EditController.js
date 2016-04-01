21a7f5bc6c75e2ebf7433c8bca9adacf8093a558
Thomas David Kehoe <kehoe@casafuturatech.com>
2016-03-31 20:53:46 -0600

    Edit working, page redirect not working.

--- public/javascript/controllers/EditController.js
+++ public/javascript/controllers/EditController.js
@@ -1,40 +1,19 @@
 app.controller('EditController', ["$scope", '$http', '$routeParams', '$location', '$firebaseArray', function($scope, $http, $routeParams, $location, $firebaseArray){
-  console.log("Edit controller");
+  console.log("Edit controller.");
   var ref = new Firebase("https://crudiest-firebase.firebaseio.com/");
   $scope.movies = $firebaseArray(ref);
   $scope.movies.$loaded()
   .then(function(){
     $scope.movie = $scope.movies.$getRecord($routeParams.id);
+    console.log($scope.movie);
   });
 
-  $scope.updateMovie = function(movie) {
+  $scope.updateMovie = function() {
     console.log("Updating movie.");
-    movie = {
-      movieActors: $scope.movie.movieActors,
-      movieAwards: $scope.movie.movieAwards,
-      movieCountry: $scope.movie.movieCountry,
-      movieDirector: $scope.movie.movieDirector,
-      movieGenre: $scope.movie.movieGenre,
-      movieLanguage: $scope.movie.movieLanguage,
-      movieMetascore: $scope.movie.movieMetascore,
-      moviePlot: $scope.movie.moviePlot,
-      moviePoster: $scope.movie.moviePoster,
-      movieRated: $scope.movie.movieRated,
-      movieRuntime: $scope.movie.movieRuntime,
-      movieTitle: $scope.movie.movieTitle,
-      movieWriter: $scope.movie.movieWriter,
-      movieYear: $scope.movie.movieYear,
-      movieImdbID: $scope.movie.movieImdbID,
-      movieImdbRating: $scope.movie.movieImdbRating,
-      movieImdbVotes: $scope.movie.movieImdbVotes,
-      movieLikes: 0,
-      movieTrivia: $scope.movie.movieTrivia,
-      comments: $scope.movie.comments
-    };
-    // $scope.movie = movie;
     console.log($scope.movie);
-    // NOT FUNCTIONAL
-    $scope.movies.$save(movie).then(function() {
+    console.log($scope.movies.$getRecord($routeParams.id));
+    $scope.movies.$save($routeParams.id).then(function(reference) {
+      console.log(reference);
       console.log("Movie updated.");
       $location.path( "/movies" );
     });
