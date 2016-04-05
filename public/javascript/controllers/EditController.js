549fd87906aa2b1837481426f6bc597744511255
Thomas David Kehoe <kehoe@casafuturatech.com>
2016-04-05 15:35:14 -0600

    () messages working.

--- public/javascript/controllers/EditController.js
+++ public/javascript/controllers/EditController.js
@@ -1,94 +1,71 @@
 app.controller('EditController', ["$scope", '$routeParams', '$location', '$firebaseArray', '$firebaseObject', function($scope, $routeParams, $location, $firebaseArray, $firebaseObject){
   console.log("Edit controller.");
-  $scope.watchAll = false;
-  $scope.watchTitle = false;
-  // Get all movies from the remote database
-  var ref = new Firebase("https://crudiest-firebase.firebaseio.com/");
-  $scope.movies = $firebaseArray(ref);
-  $scope.movies.$loaded()
-  .then(function(){
-    // Get one movie selected by its $id
-    $scope.movie = $scope.movies.$getRecord($routeParams.id); // works
-    // $scope.movie = $firebaseObject(ref.child($routeParams.id)); // edits don't save
-    // $scope.movieObject = $firebaseObject(ref.child($routeParams.id));
+  $scope.watchTitle = false; // Initialize value
+  $scope.watchPoster = false; // Initialize value
+  $scope.watchPlot = false; // Initialize value
+  $scope.watchTrivia = false; // Initialize value
+  $scope.watchDirector = false; // Initialize value
+  $scope.watchWriter = false; // Initialize value
+  $scope.watchActors = false; // Initialize value
+  $scope.watchYear = false; // Initialize value
+  var ref = new Firebase("https://crudiest-firebase.firebaseio.com/"); // Get all movies from the remote database
+  $scope.movies = $firebaseArray(ref); // Put movies onto local $scope
+  $scope.movies.$loaded() // Wait until movies downloaded
+  .then(function(){ // Promise
+    $scope.movie = $scope.movies.$getRecord($routeParams.id); // Get one movie selected by its $id in the URL
+    $scope.movieTitleObject = $firebaseObject(ref.child($routeParams.id).child('movieTitle')); // Make the movieTitle property into a $firebaseObject
+    $scope.moviePosterObject = $firebaseObject(ref.child($routeParams.id).child('moviePoster'));
+    $scope.moviePlotObject = $firebaseObject(ref.child($routeParams.id).child('moviePlot'));
+    $scope.movieTriviaObject = $firebaseObject(ref.child($routeParams.id).child('movieTrivia'));
+    $scope.movieDirectorObject = $firebaseObject(ref.child($routeParams.id).child('movieDirector'));
+    $scope.movieWriterObject = $firebaseObject(ref.child($routeParams.id).child('movieWriter'));
+    $scope.movieActorsObject = $firebaseObject(ref.child($routeParams.id).child('movieActors'));
+    $scope.movieYearObject = $firebaseObject(ref.child($routeParams.id).child('movieYear'));
+    $scope.movies.$watch(function(event) { // Watch the array of all movies
 
-    // Make the movieTitle property into a $firebaseObject
-    $scope.movieObject = $firebaseObject(ref.child($routeParams.id).child('movieTitle'));
-    $scope.watchTitle = false;
-    // $scope.movieTitle = $firebaseObject(ref.child($routeParams.id));
-    // console.log($scope.movieTitle);
-    // console.log($scope.watchTitle);
-    // $scope.movieTitle.$watch(function(event) {
-    //   console.log(event);
-    //   $scope.watchTitle = true; // Set watch variable to true when the user edits the movieTitle
-    //   console.log($scope.watchTitle);
-    // });
-
-    $scope.movies.$watch(function(event) {
-      console.log(event);
-      $scope.watchAll = true;
-      $scope.movieObject.$watch(function(event) {
-        console.log(event);
-        $scope.watchTitle = true;
+      $scope.movieTitleObject.$watch(function(event) { // Watch one property of one movie
+        $scope.watchTitle = true; // Show modal in view
       });
+
+      $scope.moviePosterObject.$watch(function(event) { // Watch one property of one movie
+        $scope.watchPoster = true; // Show modal in view
+      });
+
+      $scope.moviePlotObject.$watch(function(event) { // Watch one property of one movie
+        $scope.watchPlot = true; // Show modal in view
+      });
+
+      $scope.movieTriviaObject.$watch(function(event) { // Watch one property of one movie
+        $scope.watchTrivia = true; // Show modal in view
+      });
+
+      $scope.movieDirectorObject.$watch(function(event) { // Watch one property of one movie
+        $scope.watchDirector = true; // Show modal in view
+      });
+
+      $scope.movieWriterObject.$watch(function(event) { // Watch one property of one movie
+        $scope.watchWriter = true; // Show modal in view
+      });
+
+      $scope.movieActorsObject.$watch(function(event) { // Watch one property of one movie
+        $scope.watchActors = true; // Show modal in view
+      });
+
+      $scope.movieYearObject.$watch(function(event) { // Watch one property of one movie
+        $scope.watchYear = true; // Show modal in view
+      });
+
     });
-
-
   });
+
+  $scope.deleteMovie = function(movie) { // DESTROY
+    $scope.movies.$remove(movie).then(function() {
+      console.log("Movie deleted.");
+      $location.path( "/movies" );
+    }, function(error) {
+      console.log("Error, movie not deleted.");
+      console.log(error);
+    });
+  };
+
 }]);
-
-
-
-
-
-// $scope.movieDirector = $firebaseObject(ref.child($routeParams.id).child('movieDirector'));
-// console.log($scope.movieDirector);
-// $scope.watchDirector = false;
-// console.log($scope.watchDirector);
-// $scope.movieDirector.$watch(function(event) {
-//   console.log(event);
-//   $scope.watchDirector = true;
-//   console.log($scope.watchDirector);
-// });
-
-
-
-
-
-// $scope.updateMovie = function() {
-//   console.log("Updating movie.");
-//   var index = $scope.movies.$indexFor($routeParams.id);
-//   $scope.movies.$save(index).then(function(response) { // UPDATE
-//     $location.path( "/movies" );
-//     console.log("Movie updated.");
-//   }, function(error) {
-//     console.log("Error, no data returned.");
-//     console.log(error);
-//   });
-// };
-
-// $scope.deleteMovie = function(movie) { // DESTROY
-//   $scope.movies.$remove(movie).then(function() {
-//     console.log("Movie deleted.");
-//     $location.path( "/movies" );
-//   }, function(error) {
-//     console.log("Error, movie not deleted.");
-//     console.log(error);
-//   });
-// };
-
-
-
-
-// $scope.alerts = [
-//   { type: 'danger', msg: 'Oh snap! Change a few things up and try submitting again.' },
-//   { type: 'success', msg: 'Well done! You successfully read this important alert message.' }
-// ];
-
-// $scope.addAlert = function() {
-//   $scope.alerts.push({msg: 'Another alert!'});
-// };
-
-// $scope.closeAlert = function(index) {
-//   $scope.alerts.splice(index, 1);
-// };
