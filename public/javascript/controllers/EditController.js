365d0e749ea1002381f90e611c7625dcb9fd784c
Thomas David Kehoe <kehoe@casafuturatech.com>
2016-04-04 21:21:07 -0600

    () working for all and movieTitle.

--- public/javascript/controllers/EditController.js
+++ public/javascript/controllers/EditController.js
@@ -1,33 +1,94 @@
-app.controller('EditController', ["$scope", '$routeParams', '$location', '$firebaseArray', function($scope, $routeParams, $location, $firebaseArray){
+app.controller('EditController', ["$scope", '$routeParams', '$location', '$firebaseArray', '$firebaseObject', function($scope, $routeParams, $location, $firebaseArray, $firebaseObject){
   console.log("Edit controller.");
+  $scope.watchAll = false;
+  $scope.watchTitle = false;
+  // Get all movies from the remote database
   var ref = new Firebase("https://crudiest-firebase.firebaseio.com/");
   $scope.movies = $firebaseArray(ref);
   $scope.movies.$loaded()
   .then(function(){
-    $scope.movie = $scope.movies.$getRecord($routeParams.id);
-    console.log($scope.movie);
+    // Get one movie selected by its $id
+    $scope.movie = $scope.movies.$getRecord($routeParams.id); // works
+    // $scope.movie = $firebaseObject(ref.child($routeParams.id)); // edits don't save
+    // $scope.movieObject = $firebaseObject(ref.child($routeParams.id));
+
+    // Make the movieTitle property into a $firebaseObject
+    $scope.movieObject = $firebaseObject(ref.child($routeParams.id).child('movieTitle'));
+    $scope.watchTitle = false;
+    // $scope.movieTitle = $firebaseObject(ref.child($routeParams.id));
+    // console.log($scope.movieTitle);
+    // console.log($scope.watchTitle);
+    // $scope.movieTitle.$watch(function(event) {
+    //   console.log(event);
+    //   $scope.watchTitle = true; // Set watch variable to true when the user edits the movieTitle
+    //   console.log($scope.watchTitle);
+    // });
+
+    $scope.movies.$watch(function(event) {
+      console.log(event);
+      $scope.watchAll = true;
+      $scope.movieObject.$watch(function(event) {
+        console.log(event);
+        $scope.watchTitle = true;
+      });
+    });
+
+
   });
-
-  $scope.updateMovie = function() {
-    console.log("Updating movie.");
-    var index = $scope.movies.$indexFor($routeParams.id);
-    $scope.movies.$save(index).then(function(response) { // UPDATE
-      $location.path( "/movies" );
-      console.log("Movie updated.");
-    }, function(error) {
-      console.log("Error, no data returned.");
-      console.log(error);
-    });
-  };
-
-  $scope.deleteMovie = function(movie) { // DESTROY
-    $scope.movies.$remove(movie).then(function() {
-      console.log("Movie deleted.");
-      $location.path( "/movies" );
-    }, function(error) {
-      console.log("Error, movie not deleted.");
-      console.log(error);
-    });
-  };
-
 }]);
+
+
+
+
+
+// $scope.movieDirector = $firebaseObject(ref.child($routeParams.id).child('movieDirector'));
+// console.log($scope.movieDirector);
+// $scope.watchDirector = false;
+// console.log($scope.watchDirector);
+// $scope.movieDirector.$watch(function(event) {
+//   console.log(event);
+//   $scope.watchDirector = true;
+//   console.log($scope.watchDirector);
+// });
+
+
+
+
+
+// $scope.updateMovie = function() {
+//   console.log("Updating movie.");
+//   var index = $scope.movies.$indexFor($routeParams.id);
+//   $scope.movies.$save(index).then(function(response) { // UPDATE
+//     $location.path( "/movies" );
+//     console.log("Movie updated.");
+//   }, function(error) {
+//     console.log("Error, no data returned.");
+//     console.log(error);
+//   });
+// };
+
+// $scope.deleteMovie = function(movie) { // DESTROY
+//   $scope.movies.$remove(movie).then(function() {
+//     console.log("Movie deleted.");
+//     $location.path( "/movies" );
+//   }, function(error) {
+//     console.log("Error, movie not deleted.");
+//     console.log(error);
+//   });
+// };
+
+
+
+
+// $scope.alerts = [
+//   { type: 'danger', msg: 'Oh snap! Change a few things up and try submitting again.' },
+//   { type: 'success', msg: 'Well done! You successfully read this important alert message.' }
+// ];
+
+// $scope.addAlert = function() {
+//   $scope.alerts.push({msg: 'Another alert!'});
+// };
+
+// $scope.closeAlert = function(index) {
+//   $scope.alerts.splice(index, 1);
+// };
--- public/javascript/controllers/ShowController.js
+++ public/javascript/controllers/ShowController.js
@@ -1,74 +1,93 @@
-app.controller('ShowController', ['$scope', '$routeParams', '$location', '$firebaseArray', function($scope, $routeParams, $location, $firebaseArray) {
+// Synchronous $firebaseArray version, doesn't work
+// app.controller('ShowController', ['$scope', '$routeParams', '$location', '$firebaseArray', function($scope, $routeParams, $location, $firebaseArray) {
+//   console.log("Show controller.");
+//   var ref = new Firebase("https://crudiest-firebase.firebaseio.com/");
+//   $scope.movies = $firebaseArray(ref);
+//   console.log($scope.movies);
+//   console.log($routeParams.id);
+//   $scope.movie = $scope.movies.$getRecord($routeParams.id);
+//   console.log($scope.movie);
+
+// Asynchronous $firebaseArray version, works
+// app.controller('ShowController', ['$scope', '$routeParams', '$location', '$firebaseArray', function($scope, $routeParams, $location, $firebaseArray) {
+//   console.log("Show controller.");
+//   var ref = new Firebase("https://crudiest-firebase.firebaseio.com/");
+//   $scope.movies = $firebaseArray(ref);
+//   $scope.movies.$loaded()
+//   .then(function(){
+//     $scope.movie = $scope.movies.$getRecord($routeParams.id);
+//     console.log($scope.movie);
+//   }, function(error) {
+//     console.log("Error, movie array not loaded.");
+//     console.log(error);
+//   });
+
+// Synchronous $firebaseObject version, works
+app.controller('ShowController', ['$scope', '$routeParams', '$location', '$firebaseObject', function($scope, $routeParams, $location, $firebaseObject) {
   console.log("Show controller.");
   var ref = new Firebase("https://crudiest-firebase.firebaseio.com/");
-  $scope.movies = $firebaseArray(ref);
-  $scope.movies.$loaded()
-  .then(function(){
-    $scope.movie = $scope.movies.$getRecord($routeParams.id);
+  $scope.movie = $firebaseObject(ref.child($routeParams.id));
+  console.log($scope.movie);
+
+$scope.newComment = function(movie) { // full record is passed from the view
+  var comment = {
+    commentText: movie.newComment.commentText,
+    commentAuthor: movie.newComment.commentAuthor,
+    commentTimestamp: Date.now(),
+  };
+  var comments = movie.comments || [];
+  comments.push(comment); // push comment to local $scope
+  movie.newComment.commentAuthor = null; // needed to prevent autofilling fields
+  movie.newComment.commentText = null; // needed to prevent autofilling fields
+  movie.comments = comments; // saves new comment locally
+  $scope.movies.$save(movie).then(function() {
+    console.log("Comment added!");
   }, function(error) {
-    console.log("Error, movie array not loaded.");
+    console.log("Error, comment not added.");
     console.log(error);
   });
+};
 
-  $scope.newComment = function(movie) { // full record is passed from the view
-    var comment = {
-      commentText: movie.newComment.commentText,
-      commentAuthor: movie.newComment.commentAuthor,
-      commentTimestamp: Date.now(),
-    };
-    var comments = movie.comments || [];
-    comments.push(comment); // push comment to local $scope
-    movie.newComment.commentAuthor = null; // needed to prevent autofilling fields
-    movie.newComment.commentText = null; // needed to prevent autofilling fields
-    movie.comments = comments; // saves new comment locally
-    $scope.movies.$save(movie).then(function() {
-      console.log("Comment added!");
-    }, function(error) {
-      console.log("Error, comment not added.");
-      console.log(error);
-    });
-  };
+$scope.deleteComment = function(movie, comment) {
+  console.log("Deleting comment.")
+  var index = movie.comments.indexOf(comment); // find the index of the comment in the array of comments
+  movie.comments.splice(index, 1); // removes the comment from the array
+  $scope.movies.$save(movie).then(function() {
+    console.log("Comment deleted!");
+  }, function(error) {
+    console.log("Error, comment not deleted.");
+    console.log(error);
+  });
+};
 
-  $scope.deleteComment = function(movie, comment) {
-    console.log("Deleting comment.")
-    var index = movie.comments.indexOf(comment); // find the index of the comment in the array of comments
-    movie.comments.splice(index, 1); // removes the comment from the array
-    $scope.movies.$save(movie).then(function() {
-      console.log("Comment deleted!");
-    }, function(error) {
-      console.log("Error, comment not deleted.");
-      console.log(error);
-    });
-  };
+$scope.upLike = function(movie) {
+  movie.movieLikes += 1;
+  $scope.movies.$save(movie).then(function() {
+    console.log("Upliked!");
+  }, function(error) {
+    console.log("Error, movie not upliked.");
+    console.log(error);
+  });
+};
 
-  $scope.upLike = function(movie) {
-    movie.movieLikes += 1;
-    $scope.movies.$save(movie).then(function() {
-      console.log("Upliked!");
-    }, function(error) {
-      console.log("Error, movie not upliked.");
-      console.log(error);
-    });
-  };
+$scope.downLike = function(movie) {
+  movie.movieLikes -= 1;
+  $scope.movies.$save(movie).then(function() {
+    console.log("Downliked!");
+  }, function(error) {
+    console.log("Error, movie not downliked.");
+    console.log(error);
+  });
+};
 
-  $scope.downLike = function(movie) {
-    movie.movieLikes -= 1;
-    $scope.movies.$save(movie).then(function() {
-      console.log("Downliked!");
-    }, function(error) {
-      console.log("Error, movie not downliked.");
-      console.log(error);
-    });
-  };
-
-  $scope.deleteMovie = function(movie) { // DESTROY
-    $scope.movies.$remove(movie).then(function() {
-      console.log("Movie deleted.");
-      $location.path( "/movies" );
-    }, function(error) {
-      console.log("Error, movie not deleted.");
-      console.log(error);
-    });
-  };
+$scope.deleteMovie = function(movie) { // DESTROY
+  $scope.movies.$remove(movie).then(function() {
+    console.log("Movie deleted.");
+    $location.path( "/movies" );
+  }, function(error) {
+    console.log("Error, movie not deleted.");
+    console.log(error);
+  });
+};
 
 }]);
