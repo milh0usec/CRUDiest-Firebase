936d72f69502cc61893bb226276d3aa868334259
Thomas David Kehoe <kehoe@casafuturatech.com>
2016-04-30 18:00:40 -0600

    All fields show Saved! message.

--- public/javascript/controllers/ShowController.js
+++ public/javascript/controllers/ShowController.js
@@ -5,21 +5,36 @@ app.controller('ShowController', ['$scope', '$routeParams', '$location', '$fireb
   $scope.watch = {
     titleSave: false,
     titleChange: false,
-    poster: false,
-    plot: false,
-    trivia: false,
-    director: false,
-    writer: false,
-    actors: false,
-    year: false,
-    country: false,
-    language: false,
-    genre: false,
-    rated: false,
-    awards: false,
-    imdbRating: false,
-    imdbVotes: false,
-    metascore: false
+    posterSave: false,
+    posterChange: false,
+    plotSave: false,
+    plotChange: false,
+    triviaSave: false,
+    triviaChange: false,
+    directorSave: false,
+    directorChange: false,
+    writerSave: false,
+    writerChange: false,
+    actorsSave: false,
+    actorsChange: false,
+    yearSave: false,
+    yearChange: false,
+    countrySave: false,
+    countryChange: false,
+    languageSave: false,
+    languageChange: false,
+    genreSave: false,
+    genreChange: false,
+    ratedSave: false,
+    ratedChange: false,
+    awardsSave: false,
+    awardsChange: false,
+    imdbRatingSave: false,
+    imdbRatingChange: false,
+    imdbVotesSave: false,
+    imdbVotesChange: false,
+    metascoreSave: false,
+    metascoreChange: false
   };
 
   // Connect to Firebase
@@ -27,32 +42,148 @@ app.controller('ShowController', ['$scope', '$routeParams', '$location', '$fireb
 
   // Set up single movie object
   $scope.movie = $firebaseObject(ref.child($routeParams.id));
-  console.log($scope.movie);
-  console.log($scope.watch.title)
 
   // Set up auth
-  $scope.authObj = $firebaseAuth(ref);
-  var authData = $scope.authObj.$getAuth();
+  // $scope.authObj = $firebaseAuth(ref);
+  // var authData = $scope.authObj.$getAuth();
 
-  // Movie title "Saved!" message
-  $scope.movieTitleObject = $firebaseObject(ref.child($routeParams.id).child('movieTitle'));
-  $scope.movieTitleObject.$watch(function(event) { // Watch one property of one movie
+  // Shows and hides Saved! message
+  $scope.change = function(movie, prop) {
+    $scope.movie.$save(movie);
+    $scope.watch[prop + 'Change'] = true; // Show message
+    $timeout(function() {
+      $scope.watch[prop + 'Change'] = false; // Hide message
+    }, 9900);
+  };
+  
+  // It's not possible to make a single function for all the fields because $watch doesn't tell you which property changed.
+  // Watch movie object title property, show "Saved!" message
+  $firebaseObject(ref.child($routeParams.id).child('movieTitle')).$watch(function(event) {
     $scope.watch.titleSave = true; // Show message
-    console.log("Object watch fired!");
     $timeout(function() {
       $scope.watch.titleSave = false; // Hide message
     }, 9900);
   });
 
-  $scope.changeTitle = function(movie) {
-    $scope.movie.$save(movie);
-    $scope.watch.titleChange = true; // Show message
-    console.log("Title change fired!");
+  // Watch movie object poster property, show "Saved!" message
+  $firebaseObject(ref.child($routeParams.id).child('moviePoster')).$watch(function(event) {
+    $scope.watch.posterSave = true; // Show message
     $timeout(function() {
-      $scope.watch.titleChange = false; // Hide message
+      $scope.watch.posterSave = false; // Hide message
     }, 9900);
-  };
+  });
 
+  // Watch movie object plot property, show "Saved!" message
+  $firebaseObject(ref.child($routeParams.id).child('moviePlot')).$watch(function(event) {
+    $scope.watch.plotSave = true; // Show message
+    $timeout(function() {
+      $scope.watch.plotSave = false; // Hide message
+    }, 9900);
+  });
+
+  // Watch movie object trivia property, show "Saved!" message
+  $firebaseObject(ref.child($routeParams.id).child('movieTrivia')).$watch(function(event) {
+    $scope.watch.triviaSave = true; // Show message
+    $timeout(function() {
+      $scope.watch.triviaSave = false; // Hide message
+    }, 9900);
+  });
+
+  // Watch movie object director property, show "Saved!" message
+  $firebaseObject(ref.child($routeParams.id).child('movieDirector')).$watch(function(event) {
+    $scope.watch.directorSave = true; // Show message
+    $timeout(function() {
+      $scope.watch.directorSave = false; // Hide message
+    }, 9900);
+  });
+
+  // Watch movie object writer property, show "Saved!" message
+  $firebaseObject(ref.child($routeParams.id).child('movieWriter')).$watch(function(event) {
+    $scope.watch.writerSave = true; // Show message
+    $timeout(function() {
+      $scope.watch.writerSave = false; // Hide message
+    }, 9900);
+  });
+
+  // Watch movie object actors property, show "Saved!" message
+  $firebaseObject(ref.child($routeParams.id).child('movieActors')).$watch(function(event) {
+    $scope.watch.actorsSave = true; // Show message
+    $timeout(function() {
+      $scope.watch.actorsSave = false; // Hide message
+    }, 9900);
+  });
+
+  // Watch movie object year property, show "Saved!" message
+  $firebaseObject(ref.child($routeParams.id).child('movieYear')).$watch(function(event) {
+    $scope.watch.yearSave = true; // Show message
+    $timeout(function() {
+      $scope.watch.yearSave = false; // Hide message
+    }, 9900);
+  });
+
+  // Watch movie object country property, show "Saved!" message
+  $firebaseObject(ref.child($routeParams.id).child('movieCountry')).$watch(function(event) {
+    $scope.watch.countrySave = true; // Show message
+    $timeout(function() {
+      $scope.watch.countrySave = false; // Hide message
+    }, 9900);
+  });
+
+  // Watch movie object language property, show "Saved!" message
+  $firebaseObject(ref.child($routeParams.id).child('movieLanguage')).$watch(function(event) {
+    $scope.watch.languageSave = true; // Show message
+    $timeout(function() {
+      $scope.watch.languageSave = false; // Hide message
+    }, 9900);
+  });
+
+  // Watch movie object genre property, show "Saved!" message
+  $firebaseObject(ref.child($routeParams.id).child('movieGenre')).$watch(function(event) {
+    $scope.watch.genreSave = true; // Show message
+    $timeout(function() {
+      $scope.watch.genreSave = false; // Hide message
+    }, 9900);
+  });
+
+  // Watch movie object rated property, show "Saved!" message
+  $firebaseObject(ref.child($routeParams.id).child('movieRated')).$watch(function(event) {
+    $scope.watch.ratedSave = true; // Show message
+    $timeout(function() {
+      $scope.watch.ratedSave = false; // Hide message
+    }, 9900);
+  });
+
+  // Watch movie object awards property, show "Saved!" message
+  $firebaseObject(ref.child($routeParams.id).child('movieAwards')).$watch(function(event) {
+    $scope.watch.awardsSave = true; // Show message
+    $timeout(function() {
+      $scope.watch.awardsSave = false; // Hide message
+    }, 9900);
+  });
+
+  // Watch movie object IMDB rating property, show "Saved!" message
+  $firebaseObject(ref.child($routeParams.id).child('movieIMDBRating')).$watch(function(event) {
+    $scope.watch.imdbRatingSave = true; // Show message
+    $timeout(function() {
+      $scope.watch.imdbRatingSave = false; // Hide message
+    }, 9900);
+  });
+
+  // Watch movie object IMDB votes property, show "Saved!" message
+  $firebaseObject(ref.child($routeParams.id).child('movieIMDBVotes')).$watch(function(event) {
+    $scope.watch.imdbVotesSave = true; // Show message
+    $timeout(function() {
+      $scope.watch.imdbVotesSave = false; // Hide message
+    }, 9900);
+  });
+
+  // Watch movie object metascore property, show "Saved!" message
+  $firebaseObject(ref.child($routeParams.id).child('movieMetascore')).$watch(function(event) {
+    $scope.watch.metascoreSave = true; // Show message
+    $timeout(function() {
+      $scope.watch.metascoreSave = false; // Hide message
+    }, 9900);
+  });
 
 
 
